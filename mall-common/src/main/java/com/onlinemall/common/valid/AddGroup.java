84c1d919dd820c86fe3fbdcc867b4128f2d3a29e
junho16 <nchu17202113@163.com>
2022-10-10 22:08:03 +0800

    feat: JSR303校验、自定义校验、第三方服务构建

---
+++ mall-common/src/main/java/com/onlinemall/common/valid/AddGroup.java
@@ -0,0 +1,8 @@
+package com.onlinemall.common.valid;
+
+/**
+ * @author Junho
+ * @date 2022/10/9 16:21
+ */
+public interface AddGroup {
+}
---
+++ mall-third-party/src/test/java/com/onlinemall/MallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
